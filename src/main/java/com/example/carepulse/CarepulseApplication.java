33e2e1679d5afe1887446495f708dbfec3600a8b
Andar24 <andareasdamanik35@gmail.com>
2026-06-03 22:40:30 +0700

    Update CarepulseApplication.java

--- src/main/java/com/example/carepulse/CarepulseApplication.java
+++ src/main/java/com/example/carepulse/CarepulseApplication.java
@@ -2,28 +2,14 @@ package com.example.carepulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class CarepulseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarepulseApplication.class, args);
-    }
-
-    @EventListener({ApplicationReadyEvent.class})
-    public void openBrowser() {
         System.out.println("=============================================");
-        System.out.println("✅ SERVER BERHASIL MENYALA!");
-        System.out.println("🌐 Website berjalan di: http://localhost:8080");
+        System.out.println("✅ SERVER CAREPULSE BERHASIL MENYALA!");
         System.out.println("=============================================");
-
-        try {
-            // Memaksa sistem Windows untuk membuka browser bawaan
-            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080");
-        } catch (Exception e) {
-            System.out.println("⚠️ Gagal membuka browser otomatis. Sila buka browser secara manual ke http://localhost:8080");
-        }
     }
 }
